2e727ac978d92807d72721e6df50c27ddaec6bff
shwetacctech <149310316+shwetacctech@users.noreply.github.com>
2023-12-19 22:20:14 +0530

    Update OpenGLWindow.h

--- Visualizer/OpenGLWindow.h
+++ Visualizer/OpenGLWindow.h
@@ -2,57 +2,58 @@
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
 #include <QOpenGLBuffer>
-#include "Line.h"
 
 class QOpenGLTexture;
 class QOpenGLShader;
 class QOpenGLShaderProgram;
-
 class QOpenGLPaintDevice;
 
-class OpenGLWindow :public QOpenGLWidget, protected QOpenGLFunctions
-{
-    Q_OBJECT
-public:
-    OpenGLWindow(const QColor& background, QMainWindow* parent);
-    ~OpenGLWindow();
+class OpenGLWindow :public QOpenGLWidget, protected QOpenGLFunctions{
+        Q_OBJECT
+    public:
+        OpenGLWindow(const QColor& background, QMainWindow* parent);
+        ~OpenGLWindow();
 
-protected:
-    void paintGL() override;
-    void initializeGL() override;
-private:
-   // void createGeometry();
-    void reset();
-    void drawGrid(QVector<GLfloat>& vertices, QVector<GLfloat>& colors, float gridSize);
-    void fillSquare(const QVector<QVector2D>& squareVertices, const QVector3D& fillColor);
-    void bresenhamLinePixels(float x1, float y1, float x2, float y2, QVector<QVector2D>& pixelCoordinates);
-    void simpleDDA(float x1, float y1, float x2, float y2, QVector<QVector2D>& pixelVertices);
-private:
-    bool mAnimating = false;
-    QOpenGLContext* mContext = nullptr;
-    QOpenGLPaintDevice* mDevice = nullptr;
+        void setVectorOfLines(QVector<GLfloat>& vectorOfLines);
+        void setColorOfLines(QVector<GLfloat>& colorOfLines);
+        void updateData(const QVector<GLfloat>& vertices, const QVector<GLfloat>& colors, int flag);
+        void fillSquare(const QVector<QVector2D>& squareVertices, const QVector3D& fillColor);
 
-    QOpenGLShader* mVshader = nullptr;
-    QOpenGLShader* mFshader = nullptr;
-    QOpenGLShaderProgram* mProgram = nullptr;
+    protected:
+        void paintGL() override;
+        void initializeGL() override;
 
-    QList<QVector3D> mVertices;
-    QList<QVector3D> mNormals;
-    QOpenGLBuffer mVbo;
-    int mVertexAttr;
-    int mNormalAttr;
-    int mMatrixUniform;
-    QColor mBackground;
-    QMetaObject::Connection mContextWatchConnection;
+    private:
+        void reset();
 
-    GLint m_posAttr = 0;
-    GLint m_colAttr = 0;
-    GLint m_matrixUniform = 0;
-    float mFloatInputs[4];
-    Line *mLine;
-    int gridSize = 0;
-   // QInputDialog* mInputBox;
-public slots:
-    void getUserInput();
-    void getGridInput();
+    private:
+        bool mAnimating = false;
+        int mFlag = 0;
+        QOpenGLContext* mContext = nullptr;
+        QOpenGLPaintDevice* mDevice = nullptr;
+
+        QOpenGLShader* mVshader = nullptr;
+        QOpenGLShader* mFshader = nullptr;
+        QOpenGLShaderProgram* mProgram = nullptr;
+
+        QList<QVector3D> mVertices;
+        QList<QVector3D> mNormals;
+        QOpenGLBuffer mVbo;
+        int mVertexAttr;
+        int mNormalAttr;
+        int mMatrixUniform;
+        QColor mBackground;
+        QMetaObject::Connection mContextWatchConnection;
+
+
+        QVector<GLfloat> verticesOfOrignalLine;
+        QVector<GLfloat> colorOfOrignalLine;
+
+        GLint m_posAttr = 0;
+        GLint m_colAttr = 0;
+        GLint m_posAttr1 = 0;
+        GLint m_colAttr1 = 0;
+        GLint m_matrixUniform = 0;
 };
+
+
